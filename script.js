9fe19ed438af315461749b31bb80fac7b06b192a
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-03-03 21:35:48 +0100

    Finished zadanie_9_5

---
+++ script.js
@@ -0,0 +1,7 @@
+var showButton = document.getElementsByClassName('button');
+
+for (var i = 0; i <= showButton.length; i++) {
+		
+	 	showButton.innerText = 'The text inside buttons 1 2 3';
+		alert(showButton.innerText);
+	}
