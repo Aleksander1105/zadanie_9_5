68e8e2216ae610344a1f5a6f6751f1bcfbf0e349
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-03-04 13:12:44 +0100

    Zadanie_9_5 done

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ var showButton = document.getElementsByClassName('button');
 
 for (var i = 0; i <= showButton.length; i++) {
 		
-	 	showButton.innerText = 'The text inside buttons 1 2 3';
-		alert(showButton.innerText);
-	}
+	showButton[0].innerText = 'The text inside button 1';
+	alert(showButton[0].innerText);
+	showButton[1].innerText = 'The text inside button 2';
+	alert(showButton[1].innerText);
+	showButton[2].innerText = 'The text inside button 3';
+	alert(showButton[2].innerText);
+
+	break
+}
